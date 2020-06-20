103c2ebd01f77ff5e575edbf3fb4146d09b66cca
RubbaBoy <adamyarris@gmail.com>
2020-06-20 01:01:17 -0400

    Added modal component with inputs

---
+++ lib/src/components/modal/modal_component.dart
@@ -0,0 +1,86 @@
+import 'dart:async';
+import 'dart:html';
+
+import 'package:angular/angular.dart';
+
+@Component(
+  selector: 'lak-modal',
+  styleUrls: ['modal_component.css'],
+  templateUrl: 'modal_component.html',
+  directives: [
+    NgFor,
+    NgIf,
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+)
+class LakModalComponent {
+  final ChangeDetectorRef changeRef;
+
+  @Input()
+  String title;
+
+  final HtmlElement _host;
+
+  bool block = false;
+
+  Map<String, dynamic> content = {};
+  Function(Map<String, dynamic>) onConfirm;
+  Function() onCancel;
+  Map<String, Function> elementCallback;
+
+  bool _show = false;
+
+  set show(value) {
+    _show = value;
+    changeRef.markForCheck();
+  }
+
+  bool get show => _show;
+
+  LakModalComponent(this.changeRef, this._host);
+
+  void cancel() {
+    _hideStuff();
+    onCancel?.call();
+  }
+
+  void confirm() {
+    _hideStuff();
+
+    onConfirm?.call(elementCallback.map((selector, callback) => MapEntry(selector, callback(querySelector('#$selector'))))
+      ..removeWhere((key, value) => value == null));
+  }
+
+  void _hideStuff() {
+    show = false;
+    changeRef.markForCheck();
+    animationDelay(() => block = false, 150);
+  }
+
+  void _showStuff() {
+    block = true;
+    changeRef.markForCheck();
+
+    animationDelay(() => show = true, 10);
+  }
+
+  void openPrompt({Map<String, dynamic> content = const {}, Map<String, Function> elementCallback, Function(Map<String, dynamic>) onConfirm, Function() onCancel}) {
+    this.content = content;
+    this.onConfirm = onConfirm;
+    this.onCancel = onCancel;
+    this.elementCallback = elementCallback;
+    _showStuff();
+  }
+
+  void animationDelay(Function() function, int mills) {
+    Timer(Duration(milliseconds: mills), () {
+      function();
+      changeRef.markForCheck();
+    });
+  }
+
+  static final inputValue = (InputElement input) => input.value;
+
+  @override
+  operator [](key) => content[key];
+}
--- lib/src/sound_mixer/sound_mixer_component.dart
+++ lib/src/sound_mixer/sound_mixer_component.dart
@@ -1,6 +1,7 @@
-import 'dart:async';
+import 'dart:html';
 
-import 'package:LakWebsite/src/icon/icon_component.dart';
+import 'package:LakWebsite/src/components/icon/icon_component.dart';
+import 'package:LakWebsite/src/components/modal/modal_component.dart';
 import 'package:LakWebsite/src/services/objects/sound.dart';
 import 'package:LakWebsite/src/services/request_utils.dart';
 import 'package:angular/angular.dart';
@@ -10,6 +11,7 @@ import 'package:angular/angular.dart';
   styleUrls: ['sound_mixer_component.css'],
   templateUrl: 'sound_mixer_component.html',
   directives: [
+    LakModalComponent,
     IconComponent,
     NgFor,
     NgIf,
@@ -19,11 +21,14 @@ import 'package:angular/angular.dart';
   ],
 )
 class SoundMixerComponent implements OnInit {
-
   final RequestService requestService;
 
   SoundMixerComponent(this.requestService);
 
+  @ViewChild('addModal', read: LakModalComponent)
+  LakModalComponent addModal;
+
+  Sound active;
   List<Sound> sounds;
 
   @override
@@ -33,4 +38,26 @@ class SoundMixerComponent implements OnInit {
     requestService.listSounds().then((value) => sounds = value);
   }
 
+  void showPanel(Sound sound) {
+    active = sound;
+    print('Showing panel for ${sound.id} | ${sound.uri}');
+  }
+
+  void addSound() {
+    print('Adding sound!');
+
+    print('addModal = $addModal');
+    addModal.openPrompt(
+        onConfirm: (data) {
+          print('Confirming in here');
+          print('data = $data');
+        },
+        onCancel: () {
+          print('Cancelling in here');
+        },
+        elementCallback: {
+          'nameInput': LakModalComponent.inputValue,
+          'pathInput': LakModalComponent.inputValue,
+        });
+  }
 }
